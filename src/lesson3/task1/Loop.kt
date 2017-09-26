a3c1ca3c874dc9ad23c62fd6aab9648bd054b8af
Grom9941 <skv.41@ya.ru>
2017-09-27 00:31:45 +0300

    Исправленно и решение 3 урока

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -3,8 +3,7 @@
 package lesson3.task1
 
 import lesson1.task1.sqr
-import java.lang.Math.round
-import java.lang.Math.sqrt
+import java.lang.Math.*
 import kotlin.system.exitProcess
 
 /**
@@ -173,7 +172,10 @@ fun squareBetweenExists(m: Int, n: Int): Boolean {
  * sin(x) = x - x^3 / 3! + x^5 / 5! - x^7 / 7! + ...
  * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
  */
-fun sin(x: Double, eps: Double): Double = TODO()
+fun sin(x: Double, eps: Double): Double {
+    var s=1.1
+    while (abs(s.toDouble())<eps)
+}
 
 /**
  * Средняя
